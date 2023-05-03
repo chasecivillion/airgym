63c8b496ccf2e34e4e3866bf0c041ee866e2d876
Beau Kim <beaukim3@gmail.com>
2023-05-03 11:22:04 -0400

    CRUD wip

--- client-app/src/components/MyPods.js
+++ client-app/src/components/MyPods.js
@@ -1,16 +1,18 @@
-import React, {useContext} from 'react'
+import React, {useState, useContext} from 'react'
 import { IdContext } from './IdContext'
 
 function MyPods() {
 
+    const [ pods, setPods ] = useState([])
     const { userId, setUserId } = useContext(IdContext)
 
     
     fetch(`/user/${userId}`)
     .then(response => response.json())
-    .then(obj => console.log(obj))
+    .then(obj => setPods(obj))
+    // const 
   return (
-    <div>MyPods</div>
+    <div></div>
   )
 }
 
