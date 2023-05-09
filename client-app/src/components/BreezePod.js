ef467841993ff8a670343264391d790ce63f24a5
Beau Kim <beaukim3@gmail.com>
2023-05-09 09:59:01 -0400

    pod cards are styled

--- client-app/src/components/BreezePod.js
+++ client-app/src/components/BreezePod.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function BreezePod() {
   return (
-    <div className="relative p-8 ">
+    <div className="relative p-8 hover:shadow-lg transition duration-200 ease-out ">
       <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
         <div className="h-full w-full">
           <img className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-4 group-hover:scale-125" src="https://images.pexels.com/photos/933877/pexels-photo-933877.jpeg?cs=srgb&dl=pexels-mica-asato-933877.jpg&fm=jpg" alt="breeze pod" />
         </div>
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-        <div className="absolute inset-0 flex translate-y-[52%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+        <div className="absolute inset-0 flex translate-y-[45%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
           <h1 className="font-sans text-3xl font-bold text-white">Breeze Pod</h1>
           <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">DESCRIPTION HERE</p>
           <button id="breeze" className="rounded-full bg-neutral-900 py-2 px-3.5 font-sans text-sm capitalize text-white shadow shadow-black/60">See More</button>
--- client-app/src/components/VaporPod.js
+++ client-app/src/components/VaporPod.js
@@ -8,7 +8,7 @@ function VaporPod() {
           <img className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-4 group-hover:scale-125" src="https://i.imgur.com/DxVYr5O.jpeg" alt="vapor pod" />
         </div>
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-        <div className="absolute inset-0 flex translate-y-[52%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+        <div className="absolute inset-0 flex translate-y-[45%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
           <h1 className="font-sans text-3xl font-bold text-white">Vapor Pod</h1>
           <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">DESCRIPTION HERE</p>
           <button id="vapor" className="rounded-full bg-neutral-900 py-2 px-3.5 font-sans text-sm capitalize text-white shadow shadow-black/60">See More</button>
